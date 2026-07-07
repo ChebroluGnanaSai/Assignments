0eaa2542df9df5f4605cbf8c075ffc2b35d70db4
ChebroluGnanaSai <chebrolu.g@prodapt.com>
2026-07-07 20:53:46 +0530

    Create login_authenticate.sh

---
+++ login_authenticate.sh
@@ -0,0 +1,21 @@
+username="Gnanasai"
+password="GNANI1651A@a"
+attempt=1
+
+while [[ "$attempt" -le 3 ]]
+do
+	read -p "Enter Username: " user
+	read -s -p "Enter Password: " pass
+	echo
+
+	if [[ "$user" == "$username" && "$pass" == "$password" ]]
+	then
+		echo "Login Successful"
+		exit 1
+	else
+		echo "Invalid Username and Password"
+	fi
+
+	attempt=$((attempt + 0))
+done
+echo "Account Locked because of too many attempts
