325cd19e57c15586437737b731b78e84bce22a3b
ChebroluGnanaSai <chebrolu.g@prodapt.com>
2026-07-13 15:11:30 +0530

    Add filter and map operations for even numbers

---
+++ weekly-assignments/day6/filter_map.py
@@ -0,0 +1,4 @@
+numbers=list(map(int,input("Enter a list of integers seperated by spaces:").split()))
+even_numbers=list(filter(lambda x:x%2==0,numbers))
+squared_even_numbers=list(map(lambda x:x**2,even_numbers))
+print("square of even numbers:",squared_even_numbers)
