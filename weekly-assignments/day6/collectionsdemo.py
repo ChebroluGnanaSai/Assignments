e56c3d92414458a1044a546e765fddcf5d33bfb5
ChebroluGnanaSai <chebrolu.g@prodapt.com>
2026-07-13 15:26:07 +0530

    Create collectionsdemo.py

---
+++ weekly-assignments/day6/collectionsdemo.py
@@ -0,0 +1,5 @@
+#used to count occurrences of elements in a list or other iterable
+from collections import Counter
+data=['apple','banana','orange','apple','banana','apple']
+counter=Counter(data)
+print(counter)
